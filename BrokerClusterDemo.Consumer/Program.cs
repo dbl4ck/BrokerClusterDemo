0f840b633011838620de2f74173b6672a904247c
dbl4ck <48036107+dbl4ck@users.noreply.github.com>
2020-01-30 22:48:09 +0000

    init

---
+++ BrokerClusterDemo.Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Apache.NMS;
+using Apache.NMS.Util;
+using Apache.NMS.ActiveMQ;
+
+namespace BrokerClusterDemo.Consumer
+{
+    class Program
+    {
+        private static string window_title = "Consumer";
+        private static double loop_time = 10;
+        private static string url = "failover:(tcp://localhost:61616,tcp://localhost:61617,tcp://localhost:61618)?randomize=false";
+        private static string queueName = "queue://queue1";
+
+        static void Main(string[] args)
+        {
+            Console.Title = window_title;
+
+            IConnectionFactory factory = new ConnectionFactory(url);
+            using (IConnection connection = factory.CreateConnection())
+            using (ISession session = connection.CreateSession())
+            {
+                var queue = SessionUtil.GetQueue(session, queueName);
+                var destination = SessionUtil.GetDestination(session, queueName, DestinationType.Queue);
+                var consumer = session.CreateConsumer(destination);
+                
+                consumer.Listener += OnReceivedListener;
+
+                connection.Start();
+
+                // main loop
+                while (true)
+                {
+                    Thread.Sleep((int)loop_time);
+                }
+            }
+        }
+
+        private static void OnReceivedListener(IMessage message)
+        {
+            if (message == null)
+                return;
+
+            if ((message as ITextMessage) == null)
+                return;
+
+            Console.WriteLine(((ITextMessage)message).Text);
+        }
+    }
+}
---
+++ BrokerClusterDemo.Producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Apache.NMS;
+using Apache.NMS.Util;
+using Apache.NMS.ActiveMQ;
+
+namespace BrokerClusterDemo.Producer
+{
+    class Program
+    {
+        private static string window_title = "Producer";
+        private static double loop_time = 2000;
+        private static string url = "failover:(tcp://localhost:61616,tcp://localhost:61617,tcp://localhost:61618)?randomize=false";
+        private static string queueName = "queue://queue1";
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = window_title;
+            
+            IConnectionFactory factory = new ConnectionFactory(url);
+            using (IConnection connection = factory.CreateConnection())
+            using (ISession session = connection.CreateSession())
+            {
+                var destination = SessionUtil.GetDestination(session, queueName, DestinationType.Queue);
+                var producer = session.CreateProducer(destination);
+
+                // main loop
+                while (true)
+                {
+                    var message = producer.CreateTextMessage($"hello {Guid.NewGuid().ToString().Substring(0,5)}");
+                    
+                    producer.Send(message);
+
+                    Console.WriteLine(message.Text);
+
+                    Thread.Sleep((int)loop_time);
+                }
+            }
+
+            
+        }
+    }
+}
